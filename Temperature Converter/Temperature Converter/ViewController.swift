d5eafbdab44fb70575d64f5a4ca162b42a98842b
allison <allison.dixon@ssfs.org>
2020-02-11 11:27:52 -0500

    Finished Temperature Converter

--- Temperature Converter/Temperature Converter/ViewController.swift
+++ Temperature Converter/Temperature Converter/ViewController.swift
@@ -9,28 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var fLabel: UILabel!
+    
 
     @IBOutlet weak var degreesFOrC: UITextField!
     @IBOutlet weak var celsiusLabel: UILabel!
-    @IBOutlet weak var switchButton: UISwitch!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
+        segmentedControl.addTarget(self, action: #selector((changeText)), for: .valueChanged)
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        view.addGestureRecognizer(tap)
         // Do any additional setup after loading the view.
     }
 
     @IBAction func calculateButton(_ sender: Any) {
-        if switchButton.isOn {
+        if segmentedControl.selectedSegmentIndex == 0 {
             let fahrenheit = Double(degreesFOrC.text!)
             let celsius = (5 / 9.0) * (fahrenheit! - 32)
             celsiusLabel.text = String(celsius)
         }
         else {
-            
             let celsius = Double(degreesFOrC.text!)
             let fahrenheit = ((9 / 5.0) * celsius!) + 32
             celsiusLabel.text = String(fahrenheit)
         }
     }
-    
+    @objc func changeText() {
+        if segmentedControl.selectedSegmentIndex == 1 {
+            fLabel.text = "Celsius"
+            celsiusLabel.text = "Fahrenheit"
+        }
+        else {
+            fLabel.text = "Fahrenheit"
+            celsiusLabel.text = "Celsius"
+        }
+    }
 }
 
