2f67e03ca1e2eb61a5589104a4ebaa69faa91d8e
Allison Dixon <Allison@AdminisatorsAir.fios-router.home>
2020-04-01 14:34:17 -0400

    Finished Apple Pie

---
+++ Apple Pie/Apple Pie/Game.swift
@@ -0,0 +1,33 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Allison Dixon on 4/1/20.
+//  Copyright © 2020 Allison Dixon. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+      guessedLetters.append(letter)
+      if !word.contains(letter) {
+        incorrectMovesRemaining -= 1
+      }
+    }
+    var formattedWord: String {
+    var guessedWord = ""
+    for letter in word {
+        if guessedLetters.contains(letter) {
+            guessedWord += "\(letter)"
+        } else {
+            guessedWord += "_"
+        }
+    }
+    return guessedWord
+}
+}
