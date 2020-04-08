4ef5aadb11fc166a604d87ec8b6952f1be18f284
Allison Dixon <Allison@AdminisatorsAir.fios-router.home>
2020-04-08 11:11:51 -0400

    Added bar buttons, secondsview controller, and working on navigation

---
+++ Time It/Time It/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Time It
+//
+//  Created by Allison Dixon on 4/7/20.
+//  Copyright © 2020 Allison Dixon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
